b7225de01555c6786490a32727d57ff5203343f5
Jorge Calero <jorge.calero@gmail.com>
2023-01-26 14:25:57 +0100

    Bootstrap installed and made basic form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'EjerDirectivas';
+  titulo = 'Ejercicio de Directivas';
+  mensaje = "";
+  registrado = false;
+  nombre:string = "";
+  password:string = "";
+  entradas:Array<{titulo:string}>;
+
+  constructor(){
+    this.entradas = [
+      {titulo: "Python nos gusta cada vez mas"},
+      {titulo: "Java the best"},
+      {titulo: "JavaScript cada vez mas funcional"},
+      {titulo: "Ionic potensia pa tu carro"},
+      {titulo: "Que pasa con C++?"}
+    ];
+  }
+
+  registrarUsuario(){
+    this.registrado = true;
+    this.mensaje = "El usuario " + this.nombre + " se ha registrado con exito";
+  }
+
 }
